d3e5a3685eee7e6d29f52660f65b8c99d8a163db
Conary36 <cbeckford144@gmail.com>
2019-12-15 21:27:54 -0500

    essential files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
+import WelcomePage from "./WelcomePage";
+import CharacterList from "./CharacterList";
+
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+
+      <Link to='/'>Welcome</Link>
+      <Link to='/characters'>characters</Link>
+
+      <Route exact path='/' component={WelcomePage}></Route>
+      <Route exact path='/characters' component={CharacterList}></Route>
     </header>
+
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm({ searchTerms, handleChange }) {
+
+
   return (
-    <section className="search-form">
-     // Add a search form here
+    <section className='search-form'>
+      <form>
+        <label htmlFor="name">Search:</label>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="search"
+          onChange={handleChange}
+          value={searchTerms}
+        />
+
+      </form>
     </section>
-  );
+  )
 }
+
