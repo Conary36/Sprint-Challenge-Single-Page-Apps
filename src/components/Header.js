e99161de63076398759aa693a248099f34793f8a
Conary36 <cbeckford144@gmail.com>
2019-12-15 22:21:36 -0500

    added data  request to search form

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from 'react-router-dom';
 import WelcomePage from "./WelcomePage";
 import CharacterList from "./CharacterList";
+import SearchForm from "./SearchForm";
 
 
 export default function Header() {
@@ -10,10 +11,12 @@ export default function Header() {
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
 
       <Link to='/'>Welcome</Link>
-      <Link to='/characters'>characters</Link>
+      <Link to='/characters'>Characters</Link>
+      <Link to='/search'>Search</Link>
 
       <Route exact path='/' component={WelcomePage}></Route>
       <Route exact path='/characters' component={CharacterList}></Route>
+      <Route exact path='/search' component={SearchForm}></Route>
     </header>
 
   );
